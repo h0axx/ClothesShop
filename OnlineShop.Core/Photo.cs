30ccb968081bd1da33c61cca23ea6835f27e7c82
h0axx <karolmarkiewicz996@wp.pl>
2020-11-30 20:58:15 +0100

    Deleting unnecesary files and chaning custome User class to IdentityUser class
    Refactoring, User.cs, UserRole.cs (enum) and IUserData (interface for managing user data) deleted.

--- OnlineShop.Core/Photo.cs
+++ OnlineShop.Core/Photo.cs
@@ -7,16 +7,20 @@ namespace OnlineShop.Core
 {
     public class Photo
     {
+        // Photo identification number
+        [Required]
+        public int Id { get; set; }
+        // Path to file
+        [Required]
+        public string Path { get; set; }
+        // Identification number of the product that photo belongs to
+        [Required]
+        public int ProductId { get; set; }
+
         public Photo(int productId,string path)
         {
             ProductId = productId;
             Path = path;
         }
-        [Required]
-        public string Path { get; set; }
-        [Required]
-        public int ProductId { get; set; }
-        [Required]
-        public int Id { get; set; }
     }
 }
