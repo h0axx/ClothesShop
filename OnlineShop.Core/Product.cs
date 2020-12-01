1c2ab7af7159c692369520ceffc7095e15ec87be
h0axx <karolmarkiewicz996@wp.pl>
2020-12-01 11:24:02 +0100

    TODO queued
    Bug with client side validation, that not allows to set a price to decimal number

--- OnlineShop.Core/Product.cs
+++ OnlineShop.Core/Product.cs
@@ -14,6 +14,7 @@ namespace OnlineShop.Core
         public int Id { get; set; }
         [Required, StringLength(80)]
         public string Name { get; set; }
+        //TODO Unable to type decimal numbers when creating a new product. Its probably because of client side validation
         [Required]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
