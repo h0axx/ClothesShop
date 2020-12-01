a2e21b4e9e07b7796a30774777a7afcc8af96633
h0axx <karolmarkiewicz996@wp.pl>
2020-12-01 11:28:07 +0100

    Custom authorization for list page
    Autorization for list page. Defining what content to show, based on user role. For Customer user and Admin user.

--- OnlineShop/Pages/Collection/List.cshtml.cs
+++ OnlineShop/Pages/Collection/List.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -14,19 +15,56 @@ namespace OnlineShop.Pages.Collection
     {
         private readonly IConfiguration config;
         private readonly IProductData productData;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+
         public IEnumerable<Product> Products { get; set; }
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
         [TempData]
         public string Message { get; set; }
-        public ListModel(IConfiguration config, IProductData productData)
+        public bool Admin { get; set; }
+        public ListModel(IConfiguration config, IProductData productData, 
+                        SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
         {
             this.config = config;
             this.productData = productData;
+            this.signInManager = signInManager;
+            this.userManager = userManager;
         }
         public void OnGet()
         {
             Products = productData.GetProductsByName(SearchTerm);
+
+            if (IsUserAdmin().Result)
+            {
+                Admin = true;
+            }
+            else
+            {
+                Admin = false;
+            }
+        }
+
+        private async Task<bool> IsUserAdmin()
+        {
+            var loggedUser = await userManager.GetUserAsync(User);
+
+            if (loggedUser == null)
+            {
+                return false;
+            }
+
+            var userRoles = await userManager.GetRolesAsync(loggedUser);
+
+            var userRole = userRoles[0];
+
+            if(userRole == "Admin")
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
