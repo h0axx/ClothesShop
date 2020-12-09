4aec12ab3cdcafe167cdfcb07ddc23f711594426
h0axx <karolmarkiewicz996@wp.pl>
2020-12-09 10:34:55 +0100

    Create Basket.cs

---
+++ OnlineShop.Core/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Core
+{
+    public class Basket
+    {
+        public string UserId { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Basket()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
