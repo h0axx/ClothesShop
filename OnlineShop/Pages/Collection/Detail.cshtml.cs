b0f2ebb40c7694650383af1a86673eaf07eeefa6
h0axx <karolmarkiewicz996@wp.pl>
2021-01-05 11:56:37 +0100

    Ability to add a product to user basket
    as above

--- OnlineShop/Pages/Collection/Detail.cshtml.cs
+++ OnlineShop/Pages/Collection/Detail.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OnlineShop.Core;
@@ -12,14 +13,22 @@ namespace OnlineShop.Pages.Collection
     public class DetailModel : PageModel
     {
         private readonly IProductData productData;
+        private readonly IMemberData memberData;
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
         public Product Product { get; set; }
         public IEnumerable<Photo> Photos { get; set; }
         [TempData]
         public string Message { get; set; }
         
-        public DetailModel(IProductData productData)
+        public DetailModel(IProductData productData, IMemberData memberData,
+                        UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
             this.productData = productData;
+            this.memberData = memberData;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
         }
         public IActionResult OnGet(int productId)
         {
@@ -31,6 +40,34 @@ namespace OnlineShop.Pages.Collection
                 return RedirectToPage("./NotFound");
             }
             return Page();
+        }
+        public async Task<IActionResult> OnPost(int productId)
+        {
+            Product = productData.GetById(productId);
+            Photos = productData.GetPhotosById(productId);
+            var loggedUser = await userManager.GetUserAsync(User);
+
+            if (Product == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else if ( loggedUser == null)
+            {
+                return RedirectToPage("../Account/Login");
+            }
+            else
+            {
+                var memberId = memberData.GetMemberById(loggedUser.Id).Id;
+                var result = memberData.AddToBasket(memberId, productId);
+                if (result == null)
+                {
+                    Message = "Product already in basket!";
+                    return Page();
+                }
+                Message = "Product added to basket!";
+                memberData.Commit();
+                return Page();
+            }
         }
     }
 }
