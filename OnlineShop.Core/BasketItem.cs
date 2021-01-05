b601ade1a317a2c2dd6857ed5c6c91e75314cfb3
h0axx <karolmarkiewicz996@wp.pl>
2021-01-05 11:52:00 +0100

    Basket.cs replaced by BasketItem.cs
    BasketItem consist an Id (PK), MemberId (FK) and ProductId. Because of FK with Member, item will be dropped from database when Member is deleted.

---
+++ OnlineShop.Core/BasketItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineShop.Core
+{
+    public class BasketItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int MemberId { get; set; }
+        public BasketItem(int memberId, int productId)
+        {
+            MemberId = memberId;
+            ProductId = productId;
+        }
+    }
+}
