f59e61c999635761a5c9643a1507797643d3bc4f
h0axx <karolmarkiewicz996@wp.pl>
2021-01-05 09:30:43 +0100

    Product Available property added
    boolean value available property

--- OnlineShop.Core/Product.cs
+++ OnlineShop.Core/Product.cs
@@ -15,9 +15,15 @@ namespace OnlineShop.Core
         [Required, StringLength(80)]
         public string Name { get; set; }
         //TODO Unable to type decimal numbers when creating a new product. Its probably because of client side validation
+        /*Actually the error is more complicated. You are not allowed to pass decimal values, only integers are working.
+        Example:
+        1.99 - server side validation error, 1,99 - client and server validation error*/
         [Required]
         [Column(TypeName = "decimal(18,2)")]
+        [Range(0, 9999999.99)]
         public decimal Price { get; set; }
+        [Required]
+        public bool Available { get; set; }
         public string Description { get; set; }
         public ClothingSize Size { get; set; }
         public GenderType Gender { get; set; }
