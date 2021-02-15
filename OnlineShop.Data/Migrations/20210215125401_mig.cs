1ed03522870ccc06fd304a9f9ccd39db7ef1ee5d
h0axx <karolmarkiewicz996@wp.pl>
2021-02-15 17:16:04 +0100

    misc

---
+++ OnlineShop.Data/Migrations/20210215125401_mig.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.Data.Migrations
+{
+    public partial class mig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Cost",
+                table: "Orders",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Members",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Members");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
