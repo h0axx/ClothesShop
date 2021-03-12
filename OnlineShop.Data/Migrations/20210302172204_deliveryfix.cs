c3667597385d3491c51f505e43e04d51e0605901
h0axx <karolmarkiewicz996@wp.pl>
2021-03-12 11:28:34 +0100

    db migrations

---
+++ OnlineShop.Data/Migrations/20210302172204_deliveryfix.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineShop.Data.Migrations
+{
+    public partial class deliveryfix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Delivery_DeliveryId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Delivery",
+                table: "Delivery");
+
+            migrationBuilder.RenameTable(
+                name: "Delivery",
+                newName: "Deliveries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Provider",
+                table: "Deliveries",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Deliveries",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Deliveries",
+                table: "Deliveries",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Deliveries_DeliveryId",
+                table: "Orders",
+                column: "DeliveryId",
+                principalTable: "Deliveries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Deliveries_DeliveryId",
+                table: "Orders");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Deliveries",
+                table: "Deliveries");
+
+            migrationBuilder.RenameTable(
+                name: "Deliveries",
+                newName: "Delivery");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Provider",
+                table: "Delivery",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Delivery",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Delivery",
+                table: "Delivery",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Delivery_DeliveryId",
+                table: "Orders",
+                column: "DeliveryId",
+                principalTable: "Delivery",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
